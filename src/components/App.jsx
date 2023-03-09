7c4f57d7dab164db2b1f9daf43e06475824afa4c
Loïc Calcagno <calcagnoloic93@gmail.com>
2023-03-09 13:14:42 +0100

    Components: Navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,16 @@ import { Header } from "./Header";
 const App = () => {
   return(
     <div className="font-josephin">
-      <header className="h-screen bg-no-repeat md:w-full bg-contain">
+      <header className="h-screen bg-no-repeat md:w-full bg-cover sm:bg-contain">
         <Header />
       </header>
+      <main>
+
+      </main>
+      <footer>
+        
+      </footer>
     </div>
-    
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import logo from '../assets/images/logo.svg';
+import hamburger from '../assets/images/icon-hamburger.svg';
+
+const Navbar = () => {
+    return (
+        <nav className='mb-24 sm:mb-2 md:mb-6 xl:mb-32 p-5 md:px-32 xl:px-40 flex justify-between pt-8'>
+            <img src={logo} alt="Logo loopstudio" className='w-36'/>
+            <img src={hamburger} alt="Logo menu hamburger" className='h-4 hover:cursor-pointer md:hidden'/>
+            <div className='hidden md:flex '>
+                <ul className='text-White md:flex md:gap-10'>
+                    <li>About</li>
+                    <li>Careers</li>
+                    <li>Events</li>
+                    <li>Products</li>
+                    <li>Support</li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export { Navbar }
