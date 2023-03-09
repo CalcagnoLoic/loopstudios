cbe5c00a1858eb61836aa28885790c161b03fde7
Loïc Calcagno <calcagnoloic93@gmail.com>
2023-03-09 10:13:17 +0100

    init tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'White': 'hsl(0, 0%, 100%)',
+        'Black': 'hsl(0, 0%, 0%)',
+        'Dark Gray': 'hsl(0, 0%, 55%)',
+        'Very Dark Gray': 'hsl(0, 0%, 41%)',
+      }, 
+      fontFamily: {
+        'alata': ['Alata'],
+        'josephin': ['Josephin'],
+      }, 
+      backgroundImage: {
+        'header-mobile': "url('src/assets/images/mobile/image-hero.jpg')", 
+        'header-desktop': "url('src/assets/images/desktop/image-hero.jpg')", 
+      }
+    },
+  },
+  plugins: [],
+}
